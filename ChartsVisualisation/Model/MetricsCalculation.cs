2cdb6423640d025707a016a4f5e0c053b524bf80
Andrey Agiev <agiiv0@ya.ru>
2021-10-29 20:33:45 +0300

    Added MVP pattern, fixed mistakes

---
+++ ChartsVisualisation/Model/MetricsCalculation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LinearAlgebra;
+
+namespace ChartsVisualisation
+{
+    class MetricsCalculation
+    {
+        /// <summary>
+        /// Calculate average from data.
+        /// </summary>
+        /// <param name="mathVectors">Vector</param>
+        /// <returns>Array of average specificies</returns>
+        public List<MathVector> CalculateAverage(List<List <MathVector>> mathVectors)
+        {
+            List<MathVector> listOfAverages = new List<MathVector>();
+            for (int j = 0; j < mathVectors.Count; j++)
+            {
+                MathVector tempVector = new MathVector(mathVectors[0][0].Dimensions);
+                int count = 0;
+                for (int i = 0; i < mathVectors[j].Count; i++)
+                {
+                    if (mathVectors[j][i] != null)
+                    {
+                        tempVector = (MathVector)(tempVector + mathVectors[j][i]);
+                        count++;
+                    }
+                }
+                tempVector = (MathVector)(tempVector / count);
+
+                for (int i = 0; i < mathVectors[0][0].Dimensions; i++)
+                {
+                    tempVector[i] = Math.Round(tempVector[i], 2);
+                }
+                listOfAverages.Add(tempVector);
+            }
+            
+            return listOfAverages;
+        }
+
+        /// <summary>
+        /// Calculate distance between species
+        /// </summary>
+        public List<double> CalculateDistance(List<MathVector> avgVectors)
+        {
+
+            double[] listOfDistances = new double[3];
+            int k = 0;
+            for (int i = 0; i < avgVectors.Count - 1; i++)
+            {
+                for (int j = i+1; j < avgVectors.Count; j++)
+                {
+                    listOfDistances[k] = Math.Round(avgVectors[i].CalcDistance(avgVectors[j]), 2);
+                    k++;
+                }
+            }
+            return listOfDistances.ToList();
+        }
+    }
+}
---
+++ ChartsVisualisation/Presenter/ChartsPresenter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ChartsVisualisation.View;
+using LinearAlgebra;
+
+namespace ChartsVisualisation.Presenter
+{
+    class ChartsPresenter
+    {
+        private readonly IView view;
+        private readonly IFileParser parser = new CSVFileParser();
+        private readonly MetricsCalculation calculatorMetric = new MetricsCalculation();
+        private string filename;
+
+        private List<List<double>> ConvertAvgData(List<MathVector> mathVectors)
+        {
+            List<List<double>> avgsMatrix = new List<List<double>>();     
+            foreach(MathVector vec in mathVectors)
+            {
+                List<double> temp = new List<double>();
+                for (int i = 0; i < vec.Dimensions; i++)
+                {
+                    temp.Add(vec[i]);
+                }
+                avgsMatrix.Add(temp);
+            }
+            return avgsMatrix;
+        }
+
+        public ChartsPresenter(IView view)
+        {
+            this.view = view;
+        }
+
+        public string Path { set => filename = value; get => filename; }
+
+        public void LoadFileName()
+        {
+            view.DisplayPath = filename;
+        }
+
+        public void ProcessFile()
+        {
+            Console.WriteLine(filename);
+
+            try
+            {
+                parser.ReadFile(filename);
+                List<MathVector> avgsVectors = calculatorMetric.CalculateAverage(parser.GroupedVectors);
+                List<List<double>> avgsDoubles = ConvertAvgData(avgsVectors);
+                List<double> distanceDouble = calculatorMetric.CalculateDistance(avgsVectors);
+
+                view.SetGraphs(avgsDoubles, distanceDouble, parser.IrisSpecies, parser.IrisSpecs);
+          
+
+            }
+            catch (FileFormatException)
+            {
+                view.DisplayError("Некорректное содержимое файла");
+            }
+            catch (FileNotFoundException)
+            {
+                view.DisplayError("Пустой путь к файлу");
+            }
+            catch (FileLoadException)
+            {
+                view.DisplayError("Превышена допустимая размерность файла");
+            }
+
+        }
+    }
+}
